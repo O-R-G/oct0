639869e2b1f3ee84e703d04712424ae811ecbea3
wei <wei@o-r-g.com>
2022-05-10 15:42:36 +0800

    change columns to two

--- views/main.php
+++ views/main.php
@@ -14,24 +14,25 @@ $notes = preg_replace($find, $replace, $notes);
 
 ?><section id="main">
     <div id='content'>
-    <div id='columns'>
+    <div id='columns' class="columnsDisabled">
         <div id='en'><?
             echo $body;
-        ?></div>
-        <div id='fr'><?
+        ?></div><div id='fr'><?
             echo $notes;
-        ?></div>
+        ?>
+        </div>
     </div>
-        <div id='nav'><?
-            $next = ($uri[1] == 'about') ? 'contact' : 'about'; 
-            if (!empty($item['url'])) {
-                ?><a href='/'>
-                    <img class='inline-svg' src='media/svg/x-12-k.svg'>
-                </a>
-                <a href='/<?= $next; ?>'>
-                    <img class='inline-svg' src='media/svg/arrow-right-12-k.svg'>
-                </a><?
-            }
-        ?></div>
+    <div id='nav'><?
+        $next = ($uri[1] == 'about') ? 'contact' : 'about'; 
+        if (!empty($item['url'])) {
+            ?><a href='/'>
+                <img class='inline-svg' src='media/svg/x-12-k.svg'>
+            </a>
+            <a href='/<?= $next; ?>'>
+                <img class='inline-svg' src='media/svg/arrow-right-12-k.svg'>
+            </a><?
+        }
+    ?>
+    </div>
     </div>
 </section>
