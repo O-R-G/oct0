fc9291352d8aaea7a54bb9ac74efc6a00b785e9e
wei <wei@o-r-g.com>
2022-05-31 13:48:36 +0800

    update paypal donate code

--- views/paypal.php
+++ views/paypal.php
@@ -167,16 +167,12 @@ if( strpos(trim($item['deck']), '[paypal-disabled]') === false ) {
 	}else{ ?>
 		<!-- <script src="https://www.paypalobjects.com/donate/sdk/donate-sdk.js" charset="UTF-8"></script> -->
 		<div id="donate-buy-section" class="buy-section">
-			
 			<button id="donate-btn" class="button">DONATE</button>
-			
 			<form action="https://www.paypal.com/donate" method="post" target="_top">
-	<input type="hidden" name="business" value="NYBU7J7S436MQ" />
-	<input type="hidden" name="no_recurring" value="0" />
-	<input type="hidden" name="currency_code" value="USD" />
-	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-	<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-	</form>
+				<input type="hidden" name="hosted_button_id" value="S9TNS6HPAE4X8" />
+				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+				<img alt="" border="0" src="https://www.paypal.com/en_FR/i/scr/pixel.gif" width="1" height="1" />
+			</form>
 		</div>
 	<? } ?>
 	<script>
