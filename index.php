dec465d0517c2966950ec53721df47abba0c478c
david reinfurt <reinfurt@o-r-g.com>
2024-03-28 09:30:55 -0400

    change submenu views

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $uri = explode('/', $requestclean);
 
 require_once("views/head.php");
 require_once("views/language-toggle.php");
-require_once("views/menu.php");
+// require_once("views/menu.php");
+require_once("views/menu-simple.php");
 require_once("views/logo.php");
 
 
@@ -20,7 +21,8 @@ else if($uri[1] == 'octopus-archive')
     require_once("views/octopus-archive.php");
 }
 else if($uri[2] == 'now' && count($uri) < 5 ||
-        $uri[2] == 'archive' && count($uri) < 5) {
+        $uri[2] == 'archive' && count($uri) < 5 ||
+        $uri[2] == 'editions' && count($uri) < 5) {
     require_once("views/archive.php");
 }
 else 
